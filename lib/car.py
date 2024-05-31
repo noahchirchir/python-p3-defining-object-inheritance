5bc56f35a34a3b2943a35e983690a4010df5da92
Noah chirchir <chirchirnoah37@gmail.com>
2024-05-31 19:24:41 +0300

    Modifies Lib

--- lib/car.py
+++ lib/car.py
@@ -1,4 +1,8 @@
 from vehicle import Vehicle
 
 class Car(Vehicle):
-    pass
+    def go(self):
+        return ("VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!")
+    def fill_up_tank(self):
+        return("filling up!")
+    
